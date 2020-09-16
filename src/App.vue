2d7a01bf5043329034ca3743bb198b6a9fc719fc
chenhuamu <308208549@qq.com>
2020-09-16 20:02:30 +0800

    feat：测试触发自动部署。

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to mugege's Vue.js App"/>
+    <HelloWorld msg="Mugege"/>
   </div>
 </template>
 
