77b1e98fca63ba439d091bb4acb04f46f3c33cb4
chenhuamu <308208549@qq.com>
2020-09-16 19:51:12 +0800

    feat：修改文案。

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to your's Vue.js App"/>
+    <HelloWorld msg="Welcome to mugege's Vue.js App"/>
   </div>
 </template>
 
